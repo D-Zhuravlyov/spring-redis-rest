ebda85e7d157baea649839ca588ded1711ea2697
Dmytro.Zhuravlov <dmytro.zhuravlov@litslink.com>
2021-01-20 16:04:32 +0200

    Initial commit

---
+++ src/main/java/io/github/zhuravlyov/redisrest/app/RedisRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.zhuravlyov.redisrest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/zhuravlyov/redisrest/app/RedisRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zhuravlyov.redisrest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
